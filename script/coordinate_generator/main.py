103be89fb8c1523d01cd0c633b4c9d3fbbe80382
ElectricVersion <electric.version01@gmail.com>
2025-08-07 00:35:01 -0500

    added coordinate generator script

---
+++ script/coordinate_generator/main.py
@@ -0,0 +1,23 @@
+import os
+
+from PIL import Image
+
+def generate_coordinates(in1, out):
+    print(out)
+    img1 = Image.open(in1)
+    img_out = img1.copy()
+    pixels1 = img1.load()
+    pixels_out = img_out.load()
+    for x in range(img1.size[0]):
+        for y in range(img1.size[1]):
+            pixels_out[x, y] = (x*2,y*2,0,(pixels1[x, y][3]))
+    img_out.save(out)
+
+
+if __name__ == '__main__':
+    src_dir = "in"
+    dst_dir = "out"
+    for filename1 in os.listdir(src_dir):
+        src_name1 = src_dir + '/' + filename1
+        dst_name = dst_dir + '/' + filename1
+        generate_coordinates(src_name1, dst_name)
