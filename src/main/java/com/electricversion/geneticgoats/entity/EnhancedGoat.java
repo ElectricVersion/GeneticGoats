4898542c276180fad33ad1d13afc60a5bff73df2
ElectricVersion <electric.version01@gmail.com>
2025-09-05 17:59:35 -0500

    removed unused memory module types (why did i even add those?)

--- src/main/java/com/electricversion/geneticgoats/entity/EnhancedGoat.java
+++ src/main/java/com/electricversion/geneticgoats/entity/EnhancedGoat.java
@@ -263,8 +263,7 @@ public class EnhancedGoat extends EnhancedAnimalAbstract {
             ModMemoryModuleTypes.SLEEPING.get(),
             ModMemoryModuleTypes.SEEKING_FOOD.get(), ModMemoryModuleTypes.HUNGRY.get(),
             ModMemoryModuleTypes.PAUSE_BETWEEN_EATING.get(),
-            ModMemoryModuleTypes.FOCUS_BRAIN.get(), ModMemoryModuleTypes.PAUSE_BRAIN.get(),
-            ModMemoryModuleTypes.ROOSTING.get(), ModMemoryModuleTypes.EGG_LAYING.get()
+            ModMemoryModuleTypes.FOCUS_BRAIN.get(), ModMemoryModuleTypes.PAUSE_BRAIN.get()
     );
 
 
