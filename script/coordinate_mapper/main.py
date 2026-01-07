0b70653b565a6899dbfe2954afd2be01f53a47b7
ElectricVersion <5567072+ElectricVersion@users.noreply.github.com>
2026-01-07 14:40:51 -0600

    updated coordmapper to use recursion for directories

--- script/coordinate_mapper/main.py
+++ script/coordinate_mapper/main.py
@@ -15,19 +15,33 @@ def map_coordinates(in1, in2, out):
             pixels_out[x, y] = (coords[0],coords[1],coords[2],coords[3])
     img_out.save(out)
 
+def parse_dirs(in_dir, out_dir, map_name, current_dir):
+    raw_map_name = map_name.removesuffix(".png")
+    for file_name in os.listdir(current_dir):
+        if os.path.isfile(os.path.join(current_dir, file_name)):
+            src_name = os.path.join(current_dir, file_name)
+            save_dir = os.path.join(out_dir, raw_map_name, os.path.relpath(current_dir, in_dir))
+            dst_name = os.path.join(save_dir, file_name)
+            map_coordinates(os.path.join("map", map_name), src_name, dst_name)
+        elif os.path.isdir(os.path.join(current_dir, file_name)):
+            new_current_dir = os.path.join(current_dir, file_name)
+            new_dir = os.path.join(out_dir, raw_map_name, os.path.relpath(new_current_dir, in_dir))
+            try:
+                os.mkdir(new_dir)
+            except FileExistsError:
+                print("Directory", new_dir, "already exists.")
+            parse_dirs(in_dir, out_dir, map_name, new_current_dir)
+
 
 if __name__ == '__main__':
     map_dir = "map"
-    src_dir = "in"
-    dst_dir = "out"
+    in_dir = "in"
+    out_dir = "out"
     for map_name in os.listdir(map_dir):
         map_subdir = map_name.removesuffix(".png")
         try:
-            os.mkdir(dst_dir + "/" + map_subdir)
+            os.mkdir(os.path.join(out_dir, map_subdir))
         except FileExistsError:
             print("Directory", map_subdir, "already exists.")
 
-        for file_name in os.listdir(src_dir):
-            src_name = src_dir + '/' + file_name
-            dst_name = dst_dir + '/' + map_name.removesuffix(".png") + '/' + file_name
-            map_coordinates("map/" + map_name, src_name, dst_name)
+        parse_dirs(in_dir, out_dir, map_name, in_dir)
