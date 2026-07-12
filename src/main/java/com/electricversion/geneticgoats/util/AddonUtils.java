81b7c53f3d3be81d2dd970a78b7e41738c0394c9
ElectricVersion <5567072+ElectricVersion@users.noreply.github.com>
2026-07-12 15:21:49 -0500

    added hexToInt for better texture randomization

--- src/main/java/com/electricversion/geneticgoats/util/AddonUtils.java
+++ src/main/java/com/electricversion/geneticgoats/util/AddonUtils.java
@@ -11,4 +11,26 @@ public class AddonUtils {
     public static float clamp(float number) {
         return Mth.clamp(number, 0F, 1F);
     }
+
+    // Convert a hexadecimal character into an integer within a certain range. For texture "randomization".
+    public static int hexToInt(char uuidChar, int range) {
+        return switch (uuidChar) {
+            case '1' -> 1 % range;
+            case '2' -> 2 % range;
+            case '3' -> 3 % range;
+            case '4' -> 4 % range;
+            case '5' -> 5 % range;
+            case '6' -> 6 % range;
+            case '7' -> 7 % range;
+            case '8' -> 8 % range;
+            case '9' -> 9 % range;
+            case 'a' -> 10 % range;
+            case 'b' -> 11 % range;
+            case 'c' -> 12 % range;
+            case 'd' -> 13 % range;
+            case 'e' -> 14 % range;
+            case 'f' -> 15 % range;
+            default -> 0;
+        };
+    }
 }
